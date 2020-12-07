b3dfd3bc771bce864667f71b6fbc9698c6cdeb71
tomijs1337 <bross.tomass@gmail.com>
2020-12-07 15:03:55 -0500

    Implemented more validation, added comments

--- main.py
+++ main.py
@@ -20,24 +20,33 @@ def get_arguments():
 
 
 def change_mac(interface, new_mac):
-    # print("[*] Changing MAC address for interface " + interface + " to: " + new_mac)
+    print("[*] Changing MAC address for interface " + interface + " to: " + new_mac)
 
-    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "down"])  # Inputs the command "ifconfig eth0 down" into the terminal
     subprocess.call(["ifconfig", interface, "down", "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
-    # subprocess.call(["ifconfig", interface])
 
 
 def get_current_mac(interface):
-    ifconfig_result = subprocess.check_output(["ifconfig", interface])
-    mac_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result.decode())
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])  # Saves the ifconfig output
+    mac_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result.decode())  # searches the saved file for the MAC Address
     if mac_search_result:
         return mac_search_result.group(0)
     else:
-        print("[-] Could not read MAC address.")
+        print("[-] Could not read MAC address. Try again.")
+        exit()  # stops the code from running if no MAC Address could be read.
 
 
-options_variable = get_arguments()
+def compare_mac_addresses():
+    if current_mac != previous_mac:
+        print("[*] MAC Address was changed successfully, new MAC Address is " + current_mac)
+        return 1
+    else:
+        print("[-] MAC Address did not get changed, because the new address is the same as the previous one.")
+        return 0
+
+
+options_variable = get_arguments()  # gets the arguments(variables) from options
 
 previous_mac = get_current_mac(options_variable.interface)
 print("[*] Your current MAC Address is " + str(previous_mac))
@@ -45,4 +54,6 @@ print("[*] Your current MAC Address is " + str(previous_mac))
 change_mac(options_variable.interface, options_variable.new_mac)
 
 current_mac = get_current_mac(options_variable.interface)
-print("[*] Your current MAC Address is " + str(current_mac))
+
+if compare_mac_addresses() == 0:
+    exit()
